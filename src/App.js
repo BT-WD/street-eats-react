df1e7645ef7d2a5dbaef72a7336da45cd20bbe50
Jeff Olson <jolson615@gmail.com>
2019-10-18 20:01:04 +0000

    adds components

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import Splash from './components/Splash';
 import Testimonials from './components/Testimonials'
-import ReviewCard from './components/ReviewCard';
-import reviews from './review_data';
+// import ReviewCard from './components/ReviewCard';
+// import reviews from './review_data';
 import './App.css';
 
 const App = () => {
